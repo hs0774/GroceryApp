7e897a8ae56138cd6845c6d826cb2f0b75c4adcd
Hardeep Singh <hs0774@gmail.com>
2023-11-11 15:54:54 -0500

    more files added

--- app.js
+++ app.js
@@ -9,10 +9,30 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const shopRouter = require('./routes/shop');
 
+const compression = require("compression");
+const helmet = require("helmet")
+
 const app = express();
 
-//Set up mongoose connection 
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+//Set up mongoose connection 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery",false);
 const mongoDB = process.env.MONGODB_URI
@@ -26,6 +46,7 @@ async function main(){
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression()); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
